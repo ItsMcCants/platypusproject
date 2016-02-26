b04ef780771e45cbbcaf9b046dac86c6c64c3c16
John McCants <johnmccants@Johns-MacBook-Pro.local>
2016-02-26 12:24:18 -0800

    first commit

---
+++ PlatypusProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PlatypusProject
+//
+//  Created by John McCants on 2/26/16.
+//  Copyright © 2016 John McCants. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.title = "Yo"
+    }
+
+
+
+
+}
+
